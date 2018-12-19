0dbe79bba295e77b1b8a907819aa2f6fc69d71bc
Loredana Lungu <lgloredana@yahoo.com>
2018-12-19 09:03:25 +0200

    refactor: use functional stateless component rather then a class component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,28 +3,25 @@ import { Link } from 'react-router-dom'
 import Shelf from './../components/Shelf'
 import {shelves} from './../constants/shelfType'
 
-class MainPage extends React.Component {
-    render() {
-        const { books, changeShelfHandler } =  this.props;
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                     <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        {shelves.map((shelf) => (<Shelf key={shelf.type} 
-                                                    shelfInfo={shelf} 
-                                                    shelfBooks={books}
-                                                    changeShelfHandler={changeShelfHandler}/>))}
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'><button>Add a book</button></Link>
-                </div>
-            </div>
-        )
-    }
-}
+const MainPage = (props) => {
+   return (
+       <div className="list-books">
+           <div className="list-books-title">
+               <h1>MyReads</h1>
+           </div>
+           <div className="list-books-content">
+               <div>
+                   {shelves.map((shelf) => (<Shelf key={shelf.type}
+                                                   shelfInfo={shelf}
+                                                   shelfBooks={props.books}
+                                                   changeShelfHandler={props.changeShelfHandler}/>))}
+               </div>
+           </div>
+           <div className="open-search">
+               <Link to='/search'><button>Add a book</button></Link>
+           </div>
+       </div>
+   );
+};
 
 export default MainPage
